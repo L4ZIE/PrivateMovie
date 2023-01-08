881a86fcf53cea70441a23e03ed4b477ab45d70f
ELCoche187 <mikaedling@gmail.com>
2023-01-08 10:58:24 -0300

    Some small things that we can change on the way

---
+++ src/MoviesearchController.java
@@ -0,0 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MoviesearchController {
+
+    @FXML
+    private TableColumn<?, ?> castTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> descriptionTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> genreTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> imdbTableColumn;
+
+    @FXML
+    private TextField keywordsTextField;
+
+    @FXML
+    private TableView<?> movieTableView;
+
+    @FXML
+    private TableColumn<?, ?> nameTableColumn;
+
+}
