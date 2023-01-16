a55a5a3dd90d688be885651749baa6abfee64cd2
jklaric <klaric9jakov@gmail.com>
2023-01-16 11:30:02 +0100

    Created functions for displaying movies

---
+++ src/gui/PrivateMovie.java
@@ -0,0 +1,32 @@
+import dal.SqlServerException;
+import gui.MoviesearchController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.Console;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+public class PrivateMovie extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException, SQLException, SqlServerException {
+        //Creating controller instance
+            MoviesearchController moviesearchController = new MoviesearchController();
+
+                Parent root = FXMLLoader.load(getClass().getResource("gui/Test.fxml"));
+                primaryStage.setTitle("Movies");
+                primaryStage.setScene(new Scene(root));
+                primaryStage.show();
+        //Calling update table to initialize tableview
+                moviesearchController.initialize();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
